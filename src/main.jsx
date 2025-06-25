4dd2032793438a23c68602859accf47f5db6d85e
Anshuman Garg <anshumangarg5410@gmail.com>
2025-06-25 18:38:49 +0530

    remove basename

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import store from "./app/store.js";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="/Food-Ordering-Website">
+    <BrowserRouter>
     <Provider store = {store}>
       <App />
     </Provider>
